b6856d1b1f2f94e8d50699d905cb31e721a5749a
corruptpony <tpj_ackermans@hotmail.com>
2017-09-06 13:52:56 +0200

    android compileren

---
+++ VuforiaTest/Assets/Vuforia/Scripts/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        foreach (Transform child in transform)
+            child.gameObject.SetActive(true);
+    }
+}
